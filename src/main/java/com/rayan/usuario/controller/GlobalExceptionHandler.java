65109064a9db4ed9fd4af011649be83daba797bf
Rayan <rayan.almeida0@outlook.com>
2026-08-23 01:21:35 -0300

    Adicionando GlobalException e UnauthorizedException

---
+++ src/main/java/com/rayan/usuario/controller/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.rayan.usuario.controller;
+
+import com.rayan.usuario.infrastructure.exceptions.ConflictException;
+import com.rayan.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.rayan.usuario.infrastructure.exceptions.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> handleConflictException(ConflictException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+
+}
